5930e874ba4b57e537e79e4fb5e4b67aad63015b
Gabriel Cardoso <x399939@gruposantander.com>
2021-02-18 23:27:58 -0300

    starting react components

---
+++ front/src/components/input/input.js
@@ -0,0 +1,12 @@
+import './input.css';
+export default () => (
+    <div>
+        <h4>
+            <b>how is the weather in </b>
+            <b>
+                <input type="text" className="Input" autoFocus />
+            </b>
+            <b> ? </b>
+        </h4>
+    </div>
+);
