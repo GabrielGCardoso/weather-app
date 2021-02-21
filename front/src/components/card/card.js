273613649d103a3aadc93453e5909e7cea610013
GabrielGCardoso <gabrielguimac@gmail.com>
2021-02-21 15:24:25 -0300

    loading treatment and icon

--- front/src/components/card/card.js
+++ front/src/components/card/card.js
@@ -1,14 +1,35 @@
 import './card.css';
-export default (props) => (
-    <div className="card" width={props.width} height={props.height}>
-        <div className="container">
-            <h4>
-                <b>{props.place}</b>
-            </h4>
-            <h4>
-                <b>22 &deg;</b>
-            </h4>
-            <p>Light Rain</p>
-        </div>
-    </div>
-);
+import React from 'react';
+
+export default class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        /**
+         * you can find more about on https://openweathermap.org/weather-conditions
+         */
+        this.urlIcon = `https://openweathermap.org/img/wn`;
+    }
+
+    render = () => {
+        if (this.props.loading) return <div>Loading...</div>;
+
+        return (
+            <div
+                className="card"
+                width={this.props.width}
+                height={this.props.height}
+            >
+                <div className="container">
+                    <h4>
+                        <b>{this.props.place}</b>
+                    </h4>
+                    <h4>
+                        <b>{this.props.temperature}&deg;</b>
+                    </h4>
+                    <img src={`${this.urlIcon}/${this.props.iconCode}@2x.png`} />
+                    <p>{this.props.description}</p>
+                </div>
+            </div>
+        );
+    };
+}
